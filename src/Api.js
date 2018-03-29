fe9474d2ed6563a66315656084d236d675855da0
rahul1101010082 <rahul.shukla@surmountsoft.com>
2018-03-29 17:51:29 +0530

    pre/next component

--- src/Api.js
+++ src/Api.js
@@ -2,9 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Request from 'superagent';
-import Question from './Question';
-
-
+import Demo from './demo';
 
 class Api extends React.Component {
     constructor()
@@ -18,22 +16,42 @@ class Api extends React.Component {
            nextQuestion:0,
            value : "Initaial Data"
         }
-          this.nextQuestion = this.nextQuestion.bind(this);
-          this.previousQuestion = this.previousQuestion.bind(this);
+        this.show = true;
+        this.nextQuestion = this.nextQuestion.bind(this);
+        this.previousQuestion = this.previousQuestion.bind(this);
+        this.storePreviousQuestion = this.storePreviousQuestion.bind(this);
     }
 
     nextQuestion(){
        var next=localStorage.getItem('nextQuestion');
-       this.setState({ count : this.state.count +1})
-       
        this.setState({ currentQuestion:next})
     }
 
     previousQuestion(){
       var pre=localStorage.getItem('previousQuestion');
-      
       this.setState({ currentQuestion:pre})
       localStorage.removeItem('nextQuestion');
+      this.show = true;
+    }
+
+    sum(e){
+        localStorage.setItem('nextQuestion', e.target.value);
+    }
+
+    cal(e){
+        localStorage.setItem('nextQuestion', e.target.name);
+    }
+
+    hide(){
+        this.show = false;
+    }
+
+    storePreviousQuestion(props){
+        this.state.data.map((dynamicComponent, i) => 
+            (dynamicComponent.number == this.state.currentQuestion ) ? 
+                localStorage.setItem('previousQuestion', dynamicComponent.previousQuestion)  
+            : ""
+        )
     }
 
     componentDidMount()
@@ -59,13 +77,117 @@ class Api extends React.Component {
       return (
         <div>
             <div>
+               { this.storePreviousQuestion()}
                { this.state.data.map((dynamicComponent, i) => 
-                  <Question 
-                  componentData = {dynamicComponent}  next= {this.state.currentQuestion}/>)}
 
+                  (dynamicComponent.number == this.state.currentQuestion )
+                      ?  
+                        (dynamicComponent.type == "option") ?
+                          <div>
+                            <h1>{dynamicComponent.name}</h1>
+                            <form>
+                                <label>
+                                    <input type="radio" name="true" onClick={ this.sum } value={ dynamicComponent.option[0].nextQuestion }/> { dynamicComponent.option[0].value}
+                                </label>
+                                <label>
+                                    <input type="radio" name="true" onClick={ this.sum } value={ dynamicComponent.option[1].nextQuestion } /> { dynamicComponent.option[1].value}
+                                </label>
+                            </form>
+                        </div>
 
+                        :  
+                        (dynamicComponent.type == "name") ?
+                        <div>
+                            <h1>{dynamicComponent.name}</h1>
+                            <label>
+                                <input type="text" name={ dynamicComponent.nextQuestion } onClick = {this.cal }  placeholder="First Name"/>
+                            </label>  <br/>
+                            <label>
+                                <input type="text" name={ dynamicComponent.nextQuestion } onClick = {this.cal }  placeholder="Middle Name"/>
+                            </label> <br />
+                            <label>
+                                <input type="text" name={ dynamicComponent.nextQuestion } onClick = {this.cal }  placeholder="Last Name"/>
+                            </label>
+                        </div>   
+                        : 
+                        (dynamicComponent.type == "date") ?
+                        <div>
+                            <h1>{dynamicComponent.name}</h1>
+                            <label>
+                                <input type="date" name={ dynamicComponent.nextQuestion } onClick = {this.cal }  placeholder="Select Date"/>
+                            </label> 
+                        </div>
+                        : 
+                        (dynamicComponent.type == "address") ?
+                        <div>
+                            <h1>{dynamicComponent.name}</h1>
+                            <label>
+                                <input type="text" name={ dynamicComponent.nextQuestion } onClick = {this.cal }  placeholder="Enter Address"/>
+                            </label> 
+                        </div>
+                        : 
+                        (dynamicComponent.type == "phone") ?
+                        <div>
+                            <h1>{dynamicComponent.name}</h1>
+                            <label>
+                                <input type="text" name={ dynamicComponent.nextQuestion} onClick = {this.cal }  placeholder="Enter Phone Number"/>
+                            </label> 
+                        </div>
+                        : 
+                        (dynamicComponent.type == "email") ?
+                        <div>
+                            <h1>{dynamicComponent.name}</h1>
+                             <label>
+                                <input type="text" name={ dynamicComponent.nextQuestion } onClick = {this.cal }  placeholder="Enter Email"/>
+                            </label> 
+                        </div>
+                        : 
+                        (dynamicComponent.type == "common") ?
+                        <div>
+                            <h1>{dynamicComponent.name}</h1>
+                            <label>
+                                <input type="text" name={ dynamicComponent.nextQuestion } onClick = {this.cal }  placeholder="Enter Name"/>
+                            </label> 
+                        </div>
+                        :
+                        (dynamicComponent.type == "name-child") ?
+                        <div>
+                            <h1>{dynamicComponent.name}</h1>
+                            <label>
+                                <input type="text" name={ dynamicComponent.nextQuestion } onClick = {this.cal }  placeholder="First Name"/>
+                            </label> <br/>
+                            <label>
+                                <input type="text" name={ dynamicComponent.nextQuestion } onClick = {this.cal }  placeholder="Middle Name"/>
+                            </label> <br />
+                            <label>
+                                <input type="text" name={ dynamicComponent.nextQuestion } onClick = {this.cal }  placeholder="Last Name"/>
+                            </label>
+                            <label>
+                                <input type="date" name={ dynamicComponent.nextQuestion } onClick = {this.cal }  placeholder="Last Name"/>
+                            </label>
+                        </div>
+                        :
+                        (dynamicComponent.type == "second-name-child") ?
+                        <div>
+                            { this.hide()}
+                            <Demo Parent={ this.nextQuestion } Parentpre={this.previousQuestion} />
+                        
+
+                     
+                        </div>
+                        :""
+                    : "" 
+                  )}
+                  <div>
+                  {
+                  (this.show ===true ) ? 
+                  <div>
                   <button onClick = { this.previousQuestion } > Previous</button>
                   <button onClick = { this.nextQuestion } > Next </button>
+                  </div>
+                  : ""
+                 }
+                 </div>
             </div>
         </div>
       );
--- src/demo.js
+++ src/demo.js
@@ -7,28 +7,40 @@ import Request from 'superagent';
 class Demo extends React.Component {
 	    constructor(props){
             super(props);
-            this.state = { value : 1 ,count : 0} ;
-            this.Question = this.Question.bind(this); 
+            this.show=true;
+            this.state = { value : 1 ,count : 1 , index : 2 ,show : true} ;
             this.Next = this.Next.bind(this); 
+            this.Previous = this.Previous.bind(this); 
+            this.sum = this.sum.bind(this); 
 	    }
 
-        Question(){
-            this.setState({ value: !this.state.value,count : this.state.count +1})
-          
-        }
-
         Next(){
-            this.setState({ value: 10 ,count : this.state.count +1})
+            this.setState({ value: !this.state.value,count : this.state.count + 1}) 
+            if((this.state.count%2) == 0){
+                this.setState({ index : this.state.index + 1 }) 
+            }
         }
 
-        addQuestion(props){
-    	    return (
-    	    	<div>
-    	    	    {   
-    	    	    	(this.state.value == true) ?
-    	    	    	<div>
-    	    	    	    <h1>Name of { this.state.count } child with DOB ? </h1>
-    	    	    	    <label>
+        Previous(){
+            this.setState({ value: !this.state.value, count : this.state.count -1})
+            if((this.state.count%2) == 0){
+                this.setState({ index : this.state.index - 1 }) 
+            }
+        }
+
+        sum(e){
+            localStorage.setItem('nextQuestion', e.target.value);
+            this.setState({ show: !this.state.show}) 
+        }
+
+	    render() {
+            return ( 
+            	<div>
+            	    {   
+                        (this.state.value == true) ?
+                        <div>
+                            <h1>Name of { this.state.index } child with DOB ? </h1>
+                            <label>
                                 <input type="text"   placeholder="First Name"/>
                             </label> <br/>
                             <label>
@@ -41,34 +53,35 @@ class Demo extends React.Component {
                                 <input type="date"   placeholder="Last Name"/>
                             </label>
                         </div>
-    	    	    	: 
-    	    	    	(this.state.value == false) ?
-    	    	    	<div>
-    	    	    	    <h1>Do you have another Child ? </h1>
-    	    	    	</div>
-    	    	    	: ""
-    	    	    }
-    	    	</div>
-    	    ); 
-        } 
-
-        
-
-
-	    render() {
-            return ( 
-            	<div>
-
-            	    {this.addQuestion()}  
+                        : 
+                        (this.state.value == false) ?
+                        <div>
+                            <h1>Do you have another Child ? </h1>
+                            <label>
+                                <input type="radio" name="true"  /> Yes
+                            </label>
+                            <label>
+                                <input type="radio" name="true" onClick={ this.sum } value='13' /> No
+                            </label>
+                       
+                        </div>
+                        : ""
+                    }
             	    {
-            	  
-            	    (this.state.value == true || this.state.value == false) ?
-            	      <div>
-                    <button onClick = { this.Question } >Next</button>
-                    <button onClick = { this.Next } >Pre</button>
-                     </div>
-                    : ""
-                   
+                	    (this.state.value == true || this.state.value == false) ?
+                	    <div>
+                            {
+                            (this.state.index > 2 ) ? 
+                            <button onClick = { this.Previous } >Previous</button>
+                            : <button onClick={ this.props.Parentpre.bind(this)}>Previous</button>
+                            }
+                            {
+                            (this.state.show ===true ) ? 
+                            <button onClick = { this.Next } >Next</button>
+                            : <button  onClick={ this.props.Parent.bind(this)}> Next </button>
+                            }
+                         </div>
+                        : ""  
                     }
                 </div>
             )
